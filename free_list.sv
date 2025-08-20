287743a420792a4e686d5c163bf0bdbd3ae2881b
talha <102578023+ordinaryguy22@users.noreply.github.com>
2025-08-21 00:02:10 +0500

    Update free_list.sv

--- free_list.sv
+++ free_list.sv
@@ -1,30 +1,43 @@
-// ================== Free List (circular FIFO of phys regs) ==================
-module free_list #(parameter ARCH=32, PHYS=64, PW=5)(
-  input  logic           clk, rst,
-  input  logic           alloc,       // pop
-  output logic           alloc_ok,
-  output logic [PW-1:0]  prd_alloc,
-  input  logic           free_en,     // push
-  input  logic [PW-1:0]  prd_free
+module free_list (
+    input clk, rst,
+    input push,
+    input pop,
+    input logic [6:0] push_rd,
+    output logic pop_ok,
+    output logic [6:0]  phy_rd
 );
-  localparam int N = PHYS-ARCH;
-  logic [PW-1:0] q [N-1:0];
-  logic [$clog2(N):0] head, tail, cnt;
 
-  assign alloc_ok  = (cnt!=0);
-  assign prd_alloc = q[head];
-
-  integer i;
-  always_ff @(posedge clk) begin
-    if (rst) begin
-      for (i=0;i<N;i++) 
-      begin
-        q[i] <= (ARCH+i);
-      end
-      head<=0; tail<=0; cnt<=N;
-    end else begin
-      if (alloc && alloc_ok) begin head <= (head+1==N)?0:head+1; cnt<=cnt-1; end
-      if (free_en) begin q[tail] <= prd_free; tail <= (tail+1==N)?0:tail+1; cnt<=cnt+1; end
+    logic [6:0] list [63:0];
+    logic [6:0] head, tail, cnt;
+    
+    assign pop_ok = (cnt!=0);
+    assign phy_rd = list[head];
+    
+    integer i;
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            head <= 0;
+            tail <= 0;
+            cnt <= 6'b111111;
+            for(i=0;i<64;i++) begin
+                list[i] <= 31+i;
+            end
+        end
+        else begin
+            if(pop && pop_ok) begin
+                head <= (head+1 > 6'b111111)?0:head+1;
+                cnt <= cnt - 1;
+            end
+            if(push) begin
+                list[tail] <= push_rd;
+                tail <= (tail+1 > 6'b111111)?0:tail+1;
+                cnt <= cnt + 1;
+            end            
+        end
     end
-  end
+            
 endmodule
+        
+             
+
+    
